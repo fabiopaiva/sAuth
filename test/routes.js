d4b9bd7f5aa835d92259a2df23d6bed711d0c728
David Ferreira <daminufe.ti@gmail.com>
2016-10-19 00:49:49 +0200

    Added use strict to the tests

--- test/routes.js
+++ test/routes.js
@@ -1,94 +1,96 @@
-var request = require('supertest'),
-  app = require('../src/index'),
-  jwt = require('jsonwebtoken'),
-  UserModel = require('../src/model/user');
+'use strict';
 
-describe('Routes', () => {
+const request = require('supertest'),
+    app = require('../src/index'),
+    jwt = require('jsonwebtoken'),
+    UserModel = require('../src/model/user');
 
-  before((done) => {
-    UserModel.remove({}, (err) => {
-      done();
-    });
-  });
+describe('Routes', function describeRoutes () {
 
-  describe('#Getting ping response', () => {
-    it('should return a timestamp with 13 digits and status 200', (done) => {
-      var token = jwt.sign({ }, process.env.SECRET);
-      request(app)
-        .get('/ping')
-        .set('Authorization', 'Bearer ' + token)
-        .expect(function(res) {
-          res.body.length = res.body.time.toString().length;
-          delete res.body.time;
-        })
-        .expect(200, {
-          length: 13
-        }, done);
-    });
-  });
-
-  describe('#User requests', () => {
-    it('should create an user', (done) => {
-      var token = jwt.sign({ }, process.env.SECRET);
-      request(app)
-        .put('/user')
-        .set('Authorization', 'Bearer ' + token)
-        .send({
-          name: 'Fábio Paiva',
-          email: 'fabio@paiva.info',
-          username: 'fabio',
-          password: 'fabio',
-          provider: 'local'
-        })
-        .expect(201, done);
+    before((done) => {
+        UserModel.remove({}, (err) => {
+            done();
+        });
     });
 
-    it('should remove created user', (done) => {
-      let removeUser = (user) => {
-        var token = jwt.sign({ }, process.env.SECRET);
-        request(app)
-          .delete('/user/' + user.id)
-          .set('Authorization', 'Bearer ' + token)
-          .send()
-          .expect(204, done);
-      };
-      UserModel.findOne({email: 'fabio@paiva.info'}).then(removeUser);
+    describe('#Getting ping response', () => {
+        it('should return a timestamp with 13 digits and status 200', (done) => {
+            var token = jwt.sign({}, process.env.SECRET);
+            request(app)
+                .get('/ping')
+                .set('Authorization', 'Bearer ' + token)
+                .expect(function (res) {
+                    res.body.length = res.body.time.toString().length;
+                    delete res.body.time;
+                })
+                .expect(200, {
+                    length: 13
+                }, done);
+        });
     });
-  });
 
-  describe('#Authentication', () => {
-      it('should fail an authentication for the user', (done) => {
-        request(app)
-          .post('/authenticate')
-          .send({
-            username: 'fabio',
-            password: 'fake',
-            provider: 'local'
-          })
-          .expect(401, done);
-      });
+    describe('#User requests', () => {
+        it('should create an user', (done) => {
+            var token = jwt.sign({}, process.env.SECRET);
+            request(app)
+                .put('/user')
+                .set('Authorization', 'Bearer ' + token)
+                .send({
+                    name: 'Fábio Paiva',
+                    email: 'fabio@paiva.info',
+                    username: 'fabio',
+                    password: 'fabio',
+                    provider: 'local'
+                })
+                .expect(201, done);
+        });
 
-      it('should authenticate the user', (done) => {
-        request(app)
-          .post('/authenticate')
-          .send({
-            username: 'fabio',
-            password: 'fabio',
-            provider: 'local'
-          })
-          .expect(200)
-          .expect((res) => {
-            if (!('access_token' in res.body)) throw new Error("missing access token");
-            if (!('refresh_token' in res.body)) throw new Error("missing refresh token");
-          })
-          .end(done);
-      });
-
-      it('should fail to create an user without authentication', (done) => {
-      request(app)
-        .put('/user')
-        .send({})
-        .expect(401, done);
+        it('should remove created user', (done) => {
+            let removeUser = (user) => {
+                var token = jwt.sign({}, process.env.SECRET);
+                request(app)
+                    .delete('/user/' + user.id)
+                    .set('Authorization', 'Bearer ' + token)
+                    .send()
+                    .expect(204, done);
+            };
+            UserModel.findOne({email: 'fabio@paiva.info'}).then(removeUser);
+        });
     });
+
+    describe('#Authentication', () => {
+        it('should fail an authentication for the user', (done) => {
+            request(app)
+                .post('/authenticate')
+                .send({
+                    username: 'fabio',
+                    password: 'fake',
+                    provider: 'local'
+                })
+                .expect(401, done);
+        });
+
+        it('should authenticate the user', (done) => {
+            request(app)
+                .post('/authenticate')
+                .send({
+                    username: 'fabio',
+                    password: 'fabio',
+                    provider: 'local'
+                })
+                .expect(200)
+                .expect((res) => {
+                    if (!('access_token' in res.body)) throw new Error("missing access token");
+                    if (!('refresh_token' in res.body)) throw new Error("missing refresh token");
+                })
+                .end(done);
+        });
+
+        it('should fail to create an user without authentication', (done) => {
+            request(app)
+                .put('/user')
+                .send({})
+                .expect(401, done);
+        });
     });
 });
