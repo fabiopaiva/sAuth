a9a0046ddd93b3062cb406956ff04eee59769d2b
David Ferreira <daminufe.ti@gmail.com>
2016-10-18 23:57:49 +0200

    Change identation to 4 characters, refactored local

--- src/model/user.js
+++ src/model/user.js
@@ -1,55 +1,55 @@
 var mongoose = require('mongoose'),
-  bcrypt = require('bcrypt'),
-  SALT_WORK_FACTOR = process.env.SALT_WORK_FACTOR || 10,
-  Promise = require('bluebird');
+    bcrypt = require('bcrypt'),
+    SALT_WORK_FACTOR = process.env.SALT_WORK_FACTOR || 10,
+    Promise = require('bluebird');
 
 var UserSchema = mongoose.Schema({
-  name: String,
-  username: { type: String, index: { unique: true } },
-  picture: String,
-  email: { type: String, index: { unique: true } },
-  role: { type: String, default: 'user' },
-  password: String,
-  createdAt: { type: Date, default: Date.now },
-  updateAt: Date,
-  provider: [{ type: String, required: true }],
-  providerData: mongoose.Schema.Types.Mixed
+    name: String,
+    username: {type: String, index: {unique: true}},
+    picture: String,
+    email: {type: String, index: {unique: true}},
+    role: {type: String, default: 'user'},
+    password: String,
+    createdAt: {type: Date, default: Date.now},
+    updateAt: Date,
+    provider: [{type: String, required: true}],
+    providerData: mongoose.Schema.Types.Mixed
 });
 
 UserSchema.pre('save', function (next) {
-  var user = this;
-  if (!user.isModified('password')) return next();
-  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
-    if (err) return next(err);
-    bcrypt.hash(user.password, salt, function (err, hash) {
-      if (err) return next(err);
-      user.password = hash;
-      next();
+    var user = this;
+    if (!user.isModified('password')) return next();
+    bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+        if (err) return next(err);
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) return next(err);
+            user.password = hash;
+            next();
+        });
     });
-  });
 });
 
 UserSchema.methods.comparePassword = function (candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
-    if (err) return cb(err);
-    cb(null, isMatch);
-  });
+    bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
 };
 
 UserSchema.statics.findOrCreate = (query, data) => {
-  return new Promise((resolve, reject) => {
-    mongoose.model('User').findOne(query)
-      .then((result) => {
-        if (result) {
-          resolve(result);
-        }
-        else {
-          var user = new (mongoose.model('User'))(data);
-          user.save().then(resolve).catch(reject);
-        }
-      })
-      .catch(reject);
-  });
+    return new Promise((resolve, reject) => {
+        mongoose.model('User').findOne(query)
+            .then((result) => {
+                if (result) {
+                    resolve(result);
+                }
+                else {
+                    var user = new (mongoose.model('User'))(data);
+                    user.save().then(resolve).catch(reject);
+                }
+            })
+            .catch(reject);
+    });
 };
 
 module.exports = mongoose.model('User', UserSchema);
--- src/strategy/local.js
+++ src/strategy/local.js
@@ -1,25 +1,36 @@
-var UserModel = require('../model/user'),
-  Promise = require('bluebird'),
-  config = require('../config'),
-  generator = require('./token-generator');
+'use strict';
+
+const UserModel = require('../model/user'),
+    Promise = require('bluebird'),
+    config = require('../config'),
+    generator = require('./token-generator');
 
 
-module.exports = (data) => {
-  return new Promise((resolve, reject) => {
-    if (! data.username) return reject({ error: 'Missing username' });
-    if (! data.password) return reject({ error: 'Missing password' });
+module.exports = function LocalStrategy (data) {
+    return new Promise(promise);
 
-    UserModel.findOne({ username: data.username })
-      .then((user) => {
-        user.comparePassword(data.password, (err, isMatch) => {
-          if (err) throw err;
-          if (!isMatch) {
-            return reject({ error: 'Authentication failed' });
-          };
-          let token = generator.generateAccessToken(user);
-          resolve({ access_token: token, refresh_token: generator.generateRefreshToken(token) });
-        });
-      })
-      .catch(reject);
-  });
+    function promise (resolve, reject) {
+        if (!data.username) return reject({error: 'Missing username'});
+        if (!data.password) return reject({error: 'Missing password'});
+
+        UserModel.findOne({username: data.username})
+            .then(success)
+            .catch(reject);
+
+        function success(user) {
+            user.comparePassword(data.password, (err, isMatch) => {
+                if (err) throw err;
+                if (!isMatch) {
+                    return reject({error: 'Authentication failed'});
+                }
+
+                let token = generator.generateAccessToken(user);
+
+                resolve({
+                    access_token: token,
+                    refresh_token: generator.generateRefreshToken(token)
+                });
+            });
+        }
+    }
 }
